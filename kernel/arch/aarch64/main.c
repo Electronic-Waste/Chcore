59cdf17b937f24c17c6bc67a497f332150023298
Electronic-Waste <2690692950@qq.com>
2023-04-13 01:32:08 -0700

    Finish Exercise 4

--- kernel/arch/aarch64/main.c
+++ kernel/arch/aarch64/main.c
@@ -78,7 +78,7 @@ void main(paddr_t boot_flag)
         /* Init exception vector */
         arch_interrupt_init();
         /* LAB 4 TODO BEGIN */
-
+        
         /* LAB 4 TODO END */
         kinfo("[ChCore] interrupt init finished\n");
 
@@ -99,7 +99,7 @@ void main(paddr_t boot_flag)
 #endif
 
         /* LAB 4 TODO BEGIN */
-
+        lock_kernel();
         /* LAB 4 TODO END */
         
         /* Create initial thread here, which use the `init.bin` */
@@ -124,14 +124,14 @@ void secondary_start(void)
         pmu_init();
 
         /* LAB 4 TODO BEGIN: Set the cpu_status */
-
+        cpu_status[cpuid] = cpu_run;
         /* LAB 4 TODO END */
 #ifdef CHCORE_KERNEL_TEST
         run_test();
 #endif
 
         /* LAB 4 TODO BEGIN */
-
+        
         /* LAB 4 TODO END */
 
         lock_kernel();
